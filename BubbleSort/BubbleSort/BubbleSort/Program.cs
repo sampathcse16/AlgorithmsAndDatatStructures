7211f0c5c74a641a497645f5641da0a06819e9e2
sampathcse16 <SChindirala@spiderlogic.com>
2020-08-03 08:09:47 +0545

    Implementation of Bubble sort.

---
+++ BubbleSort/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        public static int[] BubbleSort(int[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 1; j < input.Length-i; j++)
+                {
+                    if (input[j] < input[j - 1])
+                    {
+                        int temp = input[j];
+                        input[j] = input[j - 1];
+                        input[j - 1] = temp;
+                    }
+                }
+            }
+
+            return input;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] input = {1, 10, 6, 2, 4, 5};
+            int[] output = BubbleSort(input);
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                Console.WriteLine(output[i]);
+            }
+        }
+    }
+}
